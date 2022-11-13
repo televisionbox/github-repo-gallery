6281cced2da532765e0205a400a21b26b915f93f
Amy Rusan <36975332+televisionbox@users.noreply.github.com>
2022-11-13 15:26:23 -0500

    Updated username

--- js/script.js
+++ js/script.js
@@ -25,4 +25,20 @@ const displayUserInfo = function (data) {
     </div>
   `;
   overview.append(div);
+  gitRepos();
+};
+
+const gitRepos = async function () {
+  const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await fetchRepos.json();
+  displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
 };
